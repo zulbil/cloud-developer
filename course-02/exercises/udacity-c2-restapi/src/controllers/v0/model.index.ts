a8761db8379dcc09ead56e56c69d8858873fc1ae
Zulbil <alexkhang25@yahoo.fr>
2022-02-16 11:07:20 -0600

    correction in model.index.ts

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/model.index.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/model.index.ts
@@ -1,4 +1,6 @@
 import { FeedItem } from './feed/models/FeedItem';
 import { User } from './users/models/User';
+import { Person } from './person/models/Person';
+import { Dog } from './dog/models/Dog';
 
-export const V0MODELS = [ FeedItem, User ];
+export const V0MODELS = [ FeedItem, User, Person, Dog ];
